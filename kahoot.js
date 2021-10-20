1886cca151d768cfcfa557a3c7a6a7f94ddcc264
DYLOjestem <79007533+DYLOjestem@users.noreply.github.com>
2021-10-20 17:57:18 +0200

    Add files via upload

---
+++ kahoot.js
@@ -0,0 +1,4 @@
+let gameid = prompt("❓ Kahood\n🎉 Creator DYLOjestem#8596\n🔗 https://dsc.gg/quizzer\n🔗 https://cheats.dylojestem.repl.co\n❓ Paste GAMEID");
+let link = "https://create.kahoot.it/details/";
+const answers = link + gameid;
+window.open(answers, "_blank");
