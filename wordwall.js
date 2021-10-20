1e0f95b5b14e29b383a9d3dd2d51b7ab9d5ac5ca
DYLOjestem <79007533+DYLOjestem@users.noreply.github.com>
2021-10-20 17:53:11 +0200

    Add files via upload

---
+++ wordwall.js
@@ -0,0 +1,5 @@
+alert("❓ WORD WELL\n❓ Creator DYLOjestem")
+let guid = prompt("Paste guid");
+let link = "https://wordwall.net/create/editcontent?guid=";
+const answers = link + guid;
+window.open(answers, "_blank");
