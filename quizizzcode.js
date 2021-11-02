fb0a0ac9578ffb23650c8594f2fd4599c732f16f
DYLOjestem <79007533+DYLOjestem@users.noreply.github.com>
2021-11-02 13:03:57 +0100

    Add files via upload

---
+++ quizizzcode.js
@@ -0,0 +1,4 @@
+let gameid = prompt("❓ Quizizz\n🎉 Creator DYLOjestem#8596\n🔗 https://dsc.gg/quizzer\n🔗 https://cheats.dylojestem.repl.co\n❓ Paste QUIZID");
+let link = "https://quizizz.com/admin/quiz/";
+const answers = link + gameid;
+window.open(answers, "_blank");
