a3728d62fc959bde3068b1d6aae7fa292fd8b76d
DYLOjestem <79007533+DYLOjestem@users.noreply.github.com>
2021-11-02 14:11:55 +0100

    Update quizizz.js

--- quizizz.js
+++ quizizz.js
@@ -1,15 +1,17 @@
-(function() {
-    let elem = document.createElement('div');
-    elem.style = `color: #25e000; position: fixed; z-index: 2147483647; right: 0; bottom: 0; padding: 5px; cursor: pointer;`;
-    elem.textContent = 'Inject Cheat';
- 
-    elem.onclick = () => {
-        let x = document.createElement('script');
-        x.src = 'https://cdn.jsdelivr.net/gh/DYLOjestem/Cheats@main/quizizzcode.js';
-        x.onload = () => { x.remove() };
-        (document.head || document.documentElement).appendChild(x);
- 
-        elem.remove();
-    }
-    (document.body || document.documentElement).appendChild(elem);
-})();
+var elem = document.createElement("button");
+var body = document.getElementsByTagName("body")[0];
+elem.innerHTML = "Inject Cheat";
+elem.style = `color: #4DFF00; position: fixed; z-index: 2147483647; right: 0; bottom: 0; padding: 5px; cursor: pointer;`;
+elem.style.width = '200px';
+elem.style.height = '40px';
+elem.style.background = 'blue';
+elem.onclick = () => {
+    let x = document.createElement('script');
+    x.src = 'https://cdn.jsdelivr.net/gh/DYLOjestem/Cheats@main/quizizzcode.js';
+    x.onload = () => { x.remove() };
+    (document.head || document.documentElement).appendChild(x);
+
+    elem.remove();
+}
+(document.body || document.documentElement).appendChild(elem);
+elem.style.borderRadius = '1em';
