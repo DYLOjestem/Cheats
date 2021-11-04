5aa4f31fdf129901d4b8055e70900b31b33b9be1
DYLOjestem <79007533+DYLOjestem@users.noreply.github.com>
2021-11-04 11:11:30 +0100

    Update kahoot.js

--- kahoot.js
+++ kahoot.js
@@ -1,4 +1,17 @@
-let gameid = prompt("❓ Kahood\n🎉 Creator DYLOjestem#8596\n🔗 https://dsc.gg/quizzer\n🔗 https://cheats.dylojestem.repl.co\n❓ Paste GAMEID");
-let link = "https://create.kahoot.it/details/";
-const answers = link + gameid;
-window.open(answers, "_blank");
+var elem = document.createElement("button");
+var body = document.getElementsByTagName("body")[0];
+elem.innerHTML = "Inject Cheat";
+elem.style = `color: #4DFF00; position: fixed; z-index: 2147483647; right: 0; bottom: 0; padding: 5px; cursor: pointer;`;
+elem.style.width = '200px';
+elem.style.height = '40px';
+elem.style.background = 'blue';
+elem.onclick = () => {
+    let x = document.createElement('script');
+    x.src = 'https://cdn.jsdelivr.net/gh/DYLOjestem/Cheats@main/kahootcode.js';
+    x.onload = () => { x.remove() };
+    (document.head || document.documentElement).appendChild(x);
+
+    elem.remove();
+}
+(document.body || document.documentElement).appendChild(elem);
+elem.style.borderRadius = '1em';
