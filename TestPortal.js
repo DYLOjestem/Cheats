22775b1c1105c432afb15b49a1bfebb449f98936
DYLOjestem <79007533+DYLOjestem@users.noreply.github.com>
2021-11-23 20:47:40 +0100

    Add files via upload

---
+++ TestPortal.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         TestPortal Cheat
+// @namespace    https://*.testportal.pl/
+// @version      1.0.2
+// @description  TestPortal cheat!
+// @author       DYLOjestem#8596
+// @icon         https://zapodaj.net/images/9862e8daaed2e.jpg
+// @match        https://*.testportal.net/*
+// @match        https://*.testportal.pl/*
+// @match        https://*.testportal.com/*
+// @grant        none
+// ==/UserScript==
+setInterval(() => {window.startTime = new Date().getTime()},0);
