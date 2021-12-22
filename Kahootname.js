52cabfe6a4139353e283b8a8e662a912a9e6521e
DYLOjestem <79007533+DYLOjestem@users.noreply.github.com>
2021-12-22 09:18:50 +0100

    Update Kahootname.js

--- Kahootname.js
+++ Kahootname.js
@@ -1,3 +1,6 @@
 document.getElementById("nickname").setAttribute("maxlength", 9999999);
-clear()
+clear();
+document.getElementById("nickname").value = "https://dsc.gg/quizzer";
 console.log('%c[Kahoot Name] Started...','color: #bada55');
+console.log('%c[Kahoot Name] Created by DYLOjestem#8596','color: red');
+console.log("https://dsc.gg/quizzer")
